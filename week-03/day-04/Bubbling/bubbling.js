b1e9c80d425348d2f0f4a6481c37e7138e5ebd4e
Hanqing Zhao <sherry9277@gmail.com>
2019-11-01 01:34:15 +0800

    Finish bubbling & Infinit Scroll

--- week-03/day-04/Bubbling/bubbling.js
+++ week-03/day-04/Bubbling/bubbling.js
@@ -1,13 +1,42 @@
 'use strict'
 
-var image = document.querySelector('.ima-inspector');
+var image = document.querySelector('.img-inspector');
 var buttonAction = document.querySelector('nav');
 
-var x = 0;
-var y = 0;
+let x = 0; 
+let y = 0;
+let backSize = 200;
 
-let movement = () => {
-  if(buttonAction.dataset.direction == up){
-    
+image.style.backgroundPositionX = '0px';
+image.style.backgroundPositionY = '0px';
+image.style.backgroundSize = '200%';
+
+buttonAction.addEventListener('click', (event) => {
+  if(event.target.dataset.direction == 'up'){
+    y-=10;
+    image.style.backgroundPosition = (x + 'px ' + y + 'px');
   }
-}
+  if(event.target.dataset.direction == 'down'){
+    y+=10;
+    image.style.backgroundPosition = (x + 'px ' + y + 'px');
+  }
+  if(event.target.dataset.direction == 'right'){
+    x+=10;
+    image.style.backgroundPosition = (x + 'px ' + y + 'px');
+  }
+  if(event.target.dataset.direction == 'left'){
+    x-=10;
+    image.style.backgroundPosition = (x + 'px ' + y + 'px');
+  }
+})
+
+buttonAction.addEventListener('click', (event) => {
+  if(event.target.dataset.direction == 'in'){
+    backSize += 10;
+    image.style.backgroundSize = (backSize + '%');
+  }
+  if(event.target.dataset.direction == 'out'){
+    backSize -= 10;
+    image.style.backgroundSize = (backSize + '%');
+  }
+})
