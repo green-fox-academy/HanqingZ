ac713f56d88d299df21f5d367f4a32eeb2db3e58
Hanqing Zhao <sherry9277@gmail.com>
2019-11-01 01:34:15 +0800

    Finish bubbling & Initial Scroll

--- week-03/day-04/Infinit-Scroll/main.js
+++ week-03/day-04/Infinit-Scroll/main.js
@@ -1,4 +1,26 @@
 'use strict'
 
 var scrollThreshold = 300;
+const color = ['blue', 'red', 'green', 'yellow', 'purple'];
 
+var body = document.querySelector('body');
+var div = document.createElement('section');
+body.appendChild(div);
+
+let initialSize = window.innerWidth/200 * (scrollThreshold/10);
+
+function addNewSquare(number){
+  for(let i = 0; i < number; i++){
+    var square = document.createElement('div');
+    square.style.backgroundColor = color[Math.floor(Math.random()*5)];
+    div.appendChild(square);
+  }
+}
+
+let initial = addNewSquare(initialSize);
+
+document.addEventListener('scroll', function(){
+  if(window.scrollY + window.innerHeight > body.offsetHeight - scrollThreshold){
+    addNewSquare(10);
+  }
+})
