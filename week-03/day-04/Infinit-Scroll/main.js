f4a7e38822c5b51301835f06905c58137e3c9093
Hanqing Zhao <sherry9277@gmail.com>
2019-11-01 09:42:44 +0800

    Fix some bugs

--- week-03/day-04/Infinit-Scroll/main.js
+++ week-03/day-04/Infinit-Scroll/main.js
@@ -1,18 +1,20 @@
 'use strict'
 
 var scrollThreshold = 300;
-const color = ['blue', 'red', 'green', 'yellow', 'purple'];
 
 var body = document.querySelector('body');
 var div = document.createElement('section');
 body.appendChild(div);
 
-let initialSize = window.innerWidth/200 * (scrollThreshold/10);
+const initialSize = window.innerWidth/200 * (scrollThreshold/10);
 
 function addNewSquare(number){
   for(let i = 0; i < number; i++){
     var square = document.createElement('div');
-    square.style.backgroundColor = color[Math.floor(Math.random()*5)];
+    let r = Math.floor(Math.random()*255);
+    let g = Math.floor(Math.random()*255);
+    let b = Math.floor(Math.random()*255);
+    square.style.backgroundColor = `rgb(${r}, ${g}, ${b}`;
     div.appendChild(square);
   }
 }
@@ -22,5 +24,10 @@ let initial = addNewSquare(initialSize);
 document.addEventListener('scroll', function(){
   if(window.scrollY + window.innerHeight > body.offsetHeight - scrollThreshold){
     addNewSquare(10);
+  }
+  if(window.scrollTop < document.innerHeight - scrollThreshold){
+    var square = document.createElement('div');
+    square.style.backgroundColor = color[Math.floor(Math.random()*5)];
+    body.insertAdjacentElement('afterbegin', square);
   }
 })
