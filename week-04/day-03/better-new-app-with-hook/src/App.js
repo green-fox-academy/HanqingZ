bbd06f007aaf0d42ce52e19ab56b01feb6e2f8c3
Hanqing Zhao <sherry9277@gmail.com>
2019-11-06 16:35:24 +0800

    Finish new app with hook

--- week-04/day-03/better-new-app-with-hook/src/App.js
+++ week-04/day-03/better-new-app-with-hook/src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  var word = '';
+  const [countryName, setCountryName] = useState('china');
+  const [articles, setArticles] = useState([]);
+  const newapi_key = 'f5bdda68a887445bb4d26831177a0efc';
+  const newapi_url = `https://newsapi.org/v2/top-headlines?q=${countryName}&apiKey=${newapi_key}`;
+
+  let startup = () => {
+    fetch(newapi_url)
+    .then(response => response.json())
+    .then(data =>{
+      console.log(data.articles);
+      {setArticles(data.articles)
+      }
+    })
+  }
+
+  useEffect(()=> {
+    if (countryName !== '' ) {
+      startup();
+    }
+  }, [countryName])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <input type="text" className="input-text" onChange={event => word = event.target.value} />
+        <button className="button-text" onClick={() => setCountryName(word)}> Add </button>
       </header>
+      <main>
+        <ul>
+          {articles.map((e,i) => (
+            <li key={i}> {e.title} </li>
+          ))}
+        </ul>
+      </main>
     </div>
   );
 }
