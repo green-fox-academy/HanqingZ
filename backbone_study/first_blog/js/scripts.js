2e0a8f8e08d81977c916f3bbbafcce0d299f3120
Hanqing Zhao <sherry9277@gmail.com>
2020-03-04 18:02:21 +0800

    study basic for collection and view

--- backbone_study/first_blog/js/scripts.js
+++ backbone_study/first_blog/js/scripts.js
@@ -1,7 +1,53 @@
+//Backbone
 var Blog = Backbone.Model.extend({
   defaults: {
     author: '',
     title: '',
     url: ''
   }
-})
+});
+
+//Collect all objects together
+var Blogs = Backbone.Collection.extend({});
+
+var blog1 = new Blog({
+  author:'Ajax',
+  title: 'Ajax\'s Blog',
+  url: 'http://btbtt.me',
+});
+
+var blog2 = new Blog({
+  author:'Kyya',
+  title: 'Kyya\'s Blog',
+  url: 'http://btbtt.me',
+});
+
+var blogs = new Blogs([blog1, blog2]);
+
+// Backbone View for one blog
+var BlogView = Backbone.View.entend({
+  model: new Blog(),
+  tagName: 'tr',
+  initialize: function() {
+    this.template = _.template($('.blogs-list-template').html());
+  },
+  render: function() {
+    this.$el.html(this.template(this.model.toJSON()));
+  }
+});
+
+// Backbone View for all blogs
+var BlogsView = Backbone.View.extend({
+  model: blogs,
+  el: $('.blogs-list'),
+  initialize: function() {
+    this.model.on('add', this.render(), this);
+  },
+  render: function() {
+    var self = this;
+    this.$el.html('');
+    _.each(this.model.toArray(), function(blog) {
+      self.$el.append((new BlogView({model: blog})).render().$el);
+    });
+  }
+});
